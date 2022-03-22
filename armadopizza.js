8a83fd6867fc058f9a8cb918595a7a330641ec54
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2022-03-21 21:34:13 -0300

    Se completaron las tareas de la preparacion de la pizza

--- armadopizza.js
+++ armadopizza.js
@@ -1,21 +1,36 @@
-const cocinarPizza = async (minutos) => {
+
+async function amasar(tiempo) {
     return new Promise(resolve => setTimeout(() => {
-        resolve('Beep! Pizza lista!')
-    }, minutos * 1000));
+        resolve("La masa esta lista");
+    }, tiempo * 1000));
 }
 
-const amasar = async (minutos) => {
+async function leudarMasa(tiempo) {
     return new Promise(resolve => setTimeout(() => {
-        resolve('Masa lista!')
-    }, minutos * 1000));
+        resolve("La masa esta leudada");
+    }, tiempo * 1000));
 }
 
-const hacerUnaGrandeDeMuzza = async () => {
-    console.log('Preparar la masa')
-    console.log(await amasar(5))
-    console.log(await cocinarPizza(10))
-    console.log('sacar pizza')
-    console.log('pizza lista')
+async function cocinarPizza(tiempo) {
+    return new Promise(resolve => setTimeout(() => {
+        resolve(`La pizza se cocino por ${tiempo * 5} minutos`);
+    }, tiempo * 1000));
 }
 
-hacerUnaGrandeDeMuzza()
+
+async function comerPizzaMuzzarella() {
+    console.log("1 - Precalentar el horno.");
+    console.log("2 - Reunir todos los ingredientes y preparar masa.");
+    console.log(await amasar(3));
+    console.log("3 - Dejar leudar.");
+    console.log(await leudarMasa(5));
+    console.log("4 - Estirar sobre placa pizzera y pre-cocinar.");
+    console.log(await cocinarPizza(5));
+    console.log("5 - Colocar salsa, muzzarella y terminar de cocinar.");
+    console.log(await cocinarPizza(5));
+    console.log("6 - Retirar pizza del horno.");
+    console.log("7 - Realizar Corte en 8 porciones.");
+    console.log("8 - A disfrutar !! ");
+}
+
+comerPizzaMuzzarella()
