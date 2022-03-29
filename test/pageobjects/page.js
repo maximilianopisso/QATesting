301e681b4a7e364139461ad663c034a9387aab02
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2022-03-29 15:51:34 -0300

    se armo BasePage

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -7,7 +7,23 @@ module.exports = class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open (path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+    navigateTo (url) {
+        return browser.url(url)
+    }
+
+    async clickOnWebElement(element) {
+        await element.waitForClickable();
+        await element.click();
+    }
+
+    async sendKeysToWebElement(element,text) {
+        if (text !== "")
+            await element.waitForExist();
+        await element.setValue(text);
+    }
+ 
+    async getTextFromWebElement(element){
+        await element.visibilityOf();
+        return (await element.getText());
     }
 }
